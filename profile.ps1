c11c2eda2f26c933b3ee4c472292057acaae14c8
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2024-06-29 20:08:07 +0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,8 +1,5 @@
 # Display fastfetch
 fastfetch -l "Windows 7"
 
-# GUI is bloat
-function youtube { yt-dlp $args -o - | mplayer -cache 64000 -vo caca - }
-
 # oh-my-posh
 oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/microverse-power.omp.json | Invoke-Expression
